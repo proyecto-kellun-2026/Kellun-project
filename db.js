77878ec7e2f3c93fa1607fb47c432e005b86bd59
FelipeRR42 <feliperojasretamales@gmail.com>
2026-05-09 18:27:00 -0400

    prueba PUT

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ const db = new Database('datos.db');
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS logros (
-    nombreLogro         TEXT NOT NULL PRIMARY KEY,
+    idLogro             INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombreLogro         TEXT NOT NULL,
     fechaObtencion      TEXT NOT NULL,
     descripcion         TEXT NOT NULL
   )
--- index.js
+++ index.js
@@ -10,31 +10,32 @@ app.get('/logros', (req, res) => {
   res.json(logros);
 });
 
-/*
+
 // POST /logros
 app.post('/logros', (req, res) => {
-  const { nombre, instructor, creditos } = req.body;
+  const { nombreLogro, fechaObtencion, descripcion } = req.body;
   const result = db.prepare(
-    'INSERT INTO logros (nombre, instructor, creditos) VALUES (?, ?, ?)'
-  ).run(nombre, instructor, creditos);
-  res.status(201).json({ id: result.lastInsertRowid, nombre, instructor, creditos });
+    'INSERT INTO logros (nombreLogro, fechaObtencion, descripcion) VALUES (?, ?, ?)'
+  ).run(nombreLogro, fechaObtencion, descripcion);
+  res.status(201).json({ idLogro: result.lastInsertRowid, nombreLogro, fechaObtencion, descripcion });
 });
 
+/*
 // PUT /logros/:id
 app.put('/logros/:id', (req, res) => {
-  const { nombre, instructor, creditos } = req.body;
+  const { nombreLogro, fechaObtencion, descripcion } = req.body;
   const info = db.prepare(
-    'UPDATE logros SET nombre=?, instructor=?, creditos=? WHERE id=?'
-  ).run(nombre, instructor, creditos, req.params.id);
-  if (info.changes === 0) return res.status(404).json({ error: 'Curso no encontrado' });
-  res.json({ mensaje: 'Curso actualizado' });
+    'UPDATE logros SET nombreLogro=?, fechaObtencion=?, descripcion=? WHERE id=?'
+  ).run(nombreLogro, fechaObtencion, descripcion, req.params.id);
+  if (info.changes === 0) return res.status(404).json({ error: 'Logro no encontrado' });
+  res.json({ mensaje: 'Logro actualizado' });
 });
 
 // DELETE /logros/:id
 app.delete('/logros/:id', (req, res) => {
   const info = db.prepare('DELETE FROM logros WHERE id=?').run(req.params.id);
-  if (info.changes === 0) return res.status(404).json({ error: 'Curso no encontrado' });
-  res.json({ mensaje: 'Curso eliminado' });
+  if (info.changes === 0) return res.status(404).json({ error: 'Logro no encontrado' });
+  res.json({ mensaje: 'Logro eliminado' });
 });
 */
 app.listen(3000, () => {
