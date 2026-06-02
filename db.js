fba4487a4cf30b0196f304722510e5a85c81c239
FelipeRR42 <feliperojasretamales@gmail.com>
2026-06-02 08:25:34 -0400

    feat: cambios a la API Logros

--- db.js
+++ db.js
@@ -4,10 +4,11 @@ const db = new Database('datos.db');
 db.exec(`
   CREATE TABLE IF NOT EXISTS logros (
     idLogro             INTEGER PRIMARY KEY AUTOINCREMENT,
+    voluntarioId        INTEGER NOT NULL,
     nombreLogro         TEXT NOT NULL,
     fechaObtencion      TEXT NOT NULL,
     descripcion         TEXT NOT NULL,
-    icono               TEXT NOT NULL
+    UNIQUE(voluntarioId, nombreLogro)
   )
 `);
 
